df627598b97edbc908d5fd84f73028bb3c1feb99
chenyilong <luohanchenyilong@163.com>
2016-11-21 01:44:33 +0800

    add test unit to test conversation setter and getter

--- AVOS/AVOSCloudIMTests/AVIMConversationTest.m
+++ AVOS/AVOSCloudIMTests/AVIMConversationTest.m
@@ -74,6 +74,21 @@
     WAIT;
 }
 
+- (void)testConversationGetterAndSetter {
+    AVIMConversation *conversation = [self conversationForTest];
+    NSNumber *number = @(arc4random());
+    NSString *name = [number stringValue];
+    [conversation setObject:number forKey:@"number"];
+    [conversation setObject:name forKey:@"name"];
+    [conversation updateWithCallback:^(BOOL succeeded, NSError *error) {
+        XCTAssertNil(error);
+        XCTAssertEqualObjects(conversation.name, name);
+        XCTAssertEqualObjects([conversation objectForKey:@"number"], number);
+        NOTIFY;
+    }];
+    WAIT;
+}
+
 //FIXME:TEST FAILED ==> ALL XCTAssertNil FAILED
 //AVIMConvCommand should has cid
 - (void)testUpdateConversation {
