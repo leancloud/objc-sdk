26c3c85288b48754dc0ceda01436bb697b36bd8c
Tianyong Tang <tang3w@gmail.com>
2017-03-02 22:59:33 +0800

    Support to encode receipt timestamps in keyed conversation

--- AVOS/AVOSCloudIM/AVIMKeyedConversation.h
+++ AVOS/AVOSCloudIM/AVIMKeyedConversation.h
@@ -24,7 +24,9 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong, readonly, nullable) NSDate       *createAt;       // 创建时间
 @property (nonatomic, strong, readonly, nullable) NSDate       *updateAt;       // 最后更新时间
 @property (nonatomic, strong, readonly, nullable) NSDate       *lastMessageAt;  // 对话中最后一条消息的发送时间
-@property (nonatomic, strong, readonly, nullable) AVIMMessage  *lastMessage;  // 对话中最后一条消息的发送时间
+@property (nonatomic, strong, readonly, nullable) NSDate       *lastDeliveredAt; // 对话中最近的、消息抵达对方设备的回执时间
+@property (nonatomic, strong, readonly, nullable) NSDate       *lastReadAt;      // 对话中最近的、消息被接收方标记为已读的时间
+@property (nonatomic, strong, readonly, nullable) AVIMMessage  *lastMessage;    // 对话中最后一条消息
 @property (nonatomic, copy,   readonly, nullable) NSString     *name;           // 对话名字
 @property (nonatomic, strong, readonly, nullable) NSArray      *members;        // 对话参与者列表
 @property (nonatomic, strong, readonly, nullable) NSDictionary *attributes;     // 自定义属性
