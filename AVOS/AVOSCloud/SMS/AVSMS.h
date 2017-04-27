4653f8a36cc0edc34fae19e6b914d1274b92385d
Tianyong Tang <tang3w@gmail.com>
2017-04-27 13:56:15 +0800

    Add type used to hold SMS relevant APIs

---
+++ AVOS/AVOSCloud/SMS/AVSMS.h
@@ -0,0 +1,13 @@
+//
+//  AVSMS.h
+//  AVOS
+//
+//  Created by Tang Tianyong on 27/04/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVSMS : NSObject
+
+@end
---
+++ AVOS/AVOSCloud/SMS/AVSMS.m
@@ -0,0 +1,13 @@
+//
+//  AVSMS.m
+//  AVOS
+//
+//  Created by Tang Tianyong on 27/04/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "AVSMS.h"
+
+@implementation AVSMS
+
+@end
