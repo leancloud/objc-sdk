605e901a26e29235d6d192916321547112ec7493
Tianyong Tang <tang3w@gmail.com>
2017-04-27 17:15:01 +0800

    Add type used to hold explicitly defined dynamic properties

---
+++ AVOS/AVOSCloud/Utils/AVDynamicObject.h
@@ -0,0 +1,13 @@
+//
+//  AVDynamicObject.h
+//  AVOS
+//
+//  Created by Tang Tianyong on 27/04/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVDynamicObject : NSObject
+
+@end
---
+++ AVOS/AVOSCloud/Utils/AVDynamicObject.m
@@ -0,0 +1,13 @@
+//
+//  AVDynamicObject.m
+//  AVOS
+//
+//  Created by Tang Tianyong on 27/04/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "AVDynamicObject.h"
+
+@implementation AVDynamicObject
+
+@end
