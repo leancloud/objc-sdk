418718e7dd7aed21f8a8b6fa77ef1df5160d7118
Tianyong Tang <tang3w@gmail.com>
2017-04-28 16:44:40 +0800

    Improve API for request short message

--- AVOS/AVOSCloud/SMS/AVSMS.h
+++ AVOS/AVOSCloud/SMS/AVSMS.h
@@ -7,7 +7,43 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "AVDynamicObject.h"
+#import "AVConstants.h"
+
+@interface AVShortMessageRequestOptions : AVDynamicObject
+
+/// Time to live of short message, in minutes.
+@property (nonatomic, assign) NSInteger TTL;
+
+/// Type of short message. Currently support two types: "voice" and "sms", defaults to "sms".
+@property (nonatomic, copy) NSString *type;
+
+/// Template of short message.
+@property (nonatomic, copy) NSString *template;
+
+/// Signature of short message template.
+@property (nonatomic, copy) NSString *signature;
+
+/// Application name displayed in short message. If not given, the application name in console will be used.
+@property (nonatomic, copy) NSString *applicationName;
+
+/// Operation name of short message.
+@property (nonatomic, copy) NSString *operationName;
+
+@end
+
 
 @interface AVSMS : NSObject
+
+/**
+ Request a short message for a phone number.
+
+ @param phoneNumber The phone number which the short message will sent to.
+ @param options     The options that configure short message.
+ @param callback    The callback of request.
+ */
++ (void)requestShortMessageForPhoneNumber:(NSString *)phoneNumber
+                                  options:(AVShortMessageRequestOptions *)options
+                                 callback:(AVBooleanResultBlock)callback;
 
 @end
--- AVOS/AVOSCloud/SMS/AVSMS.m
+++ AVOS/AVOSCloud/SMS/AVSMS.m
@@ -7,7 +7,41 @@
 //
 
 #import "AVSMS.h"
+#import "AVPaasClient.h"
+#import "AVUtils.h"
+
+@implementation AVShortMessageRequestOptions
+
+@dynamic TTL;
+@dynamic type;
+@dynamic template;
+@dynamic signature;
+@dynamic applicationName;
+@dynamic operationName;
+
+@end
 
 @implementation AVSMS
+
++ (void)requestShortMessageForPhoneNumber:(NSString *)phoneNumber
+                                  options:(AVShortMessageRequestOptions *)options
+                                 callback:(AVBooleanResultBlock)callback
+{
+    NSParameterAssert(phoneNumber);
+
+    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+
+    parameters[@"mobilePhoneNumber"] = phoneNumber;
+    parameters[@"ttl"]               = @(options.TTL);
+    parameters[@"smsType"]           = options.type;
+    parameters[@"template"]          = options.template;
+    parameters[@"sign"]              = options.signature;
+    parameters[@"name"]              = options.applicationName;
+    parameters[@"op"]                = options.operationName;
+
+    [[AVPaasClient sharedInstance] postObject:@"requestSmsCode" withParameters:parameters block:^(id object, NSError *error) {
+        [AVUtils callBooleanResultBlock:callback error:error];
+    }];
+}
 
 @end
