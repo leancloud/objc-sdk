ded26598adb26c302c3c9d813cfbf6978884750c
Tianyong Tang <tang3w@gmail.com>
2017-05-03 13:31:08 +0800

    Add class for captcha

---
+++ AVOS/AVOSCloud/Captcha/AVCaptcha.h
@@ -0,0 +1,13 @@
+//
+//  AVCaptcha.h
+//  AVOS
+//
+//  Created by Tang Tianyong on 03/05/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVCaptcha : NSObject
+
+@end
---
+++ AVOS/AVOSCloud/Captcha/AVCaptcha.m
@@ -0,0 +1,13 @@
+//
+//  AVCaptcha.m
+//  AVOS
+//
+//  Created by Tang Tianyong on 03/05/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "AVCaptcha.h"
+
+@implementation AVCaptcha
+
+@end
