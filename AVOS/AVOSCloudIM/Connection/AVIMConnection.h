301d7ea913f2dc13d58fc91202b5714c43a97f99
Tang Tianyong <tang3w@gmail.com>
2017-06-09 17:28:39 +0800

    Remove connection implementation to prepare to refactor

--- AVOS/AVOSCloudIM/Connection/AVIMConnection.h
+++ AVOS/AVOSCloudIM/Connection/AVIMConnection.h
@@ -1,63 +1,13 @@
 //
 //  AVIMConnection.h
-//  AVOSCloudIM
+//  AVOS
 //
-//  Created by Qihe Bian on 12/4/14.
-//  Copyright (c) 2014 LeanCloud Inc. All rights reserved.
+//  Created by Tang Tianyong on 09/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
 //
 
-#import "AVIMCommon.h"
-#import "AVIMCommandCommon.h"
-
-@class AVIMConnection;
-
-@protocol AVIMConnectionDelegate <NSObject>
-
-- (void)connectionDidOpen:(AVIMConnection *)connection;
-
-- (void)connection:(AVIMConnection *)connection didReceiveCommand:(AVIMGenericCommand *)command;
-
-- (void)connection:(AVIMConnection *)connection didReceiveError:(NSError *)error;
-
-- (void)connection:(AVIMConnection *)connection didCloseWithError:(NSError *)error;
-
-- (void)connectionDidReconnect:(AVIMConnection *)connection;
-
-@end
+#import <Foundation/Foundation.h>
 
 @interface AVIMConnection : NSObject
-
-@property (nonatomic, assign) CGFloat timeout;
-
-+ (instancetype)sharedInstance;
-
-/**
- Set the default timeout interval.
-
- @param seconds Timeout length, in seconds.
- */
-+ (void)setTimeoutIntervalInSeconds:(NSTimeInterval)seconds;
-
-/**
- Add a delegate for receiving connection events.
-
- @note The connection object will weakly hold the delegate.
-
- @param delegate The object to receive connection events.
- */
-- (void)addDelegate:(id<AVIMConnectionDelegate>)delegate;
-
-/**
- Remove a delegate that added previously.
-
- @param delegate The previously added delegate.
- */
-- (void)removeDelegate:(id<AVIMConnectionDelegate>)delegate;
-
-- (void)openWithCallback:(AVIMBooleanResultBlock)callback;
-
-- (void)sendCommand:(AVIMGenericCommand *)genericCommand;
-
-- (BOOL)isOpen;
 
 @end
