24007cfc9bfdd923b08a4aed36aa563f78985859
Tang Tianyong <tang3w@gmail.com>
2017-06-10 02:01:21 +0800

    Add method to call delegate methods for connection

--- AVOS/AVOSCloudIM/Connection/AVIMConnection.m
+++ AVOS/AVOSCloudIM/Connection/AVIMConnection.m
@@ -7,6 +7,7 @@
 //
 
 #import "AVIMConnection.h"
+#import "AVMethodDispatcher.h"
 
 @interface AVIMConnection ()
 
@@ -47,6 +48,27 @@
 
 - (void)removeDelegate:(id<AVIMConnectionDelegate>)delegate {
     [_delegates removeObject:delegate];
+}
+
+- (void)callDelegateMethod:(SEL)selector
+             withArguments:(id)argument1, ...
+{
+    /* NOTE: We convert the hash table to an array
+             to avoid some quirks of hash table. */
+    NSArray *delegates = [self.delegates allObjects];
+
+    if (!delegates.count)
+        return;
+
+    va_list args;
+    va_start(args, argument1);
+
+    for (id delegate in delegates) {
+        AVMethodDispatcher *dispatcher = [[AVMethodDispatcher alloc] initWithTarget:delegate selector:selector];
+        [dispatcher callWithArgument:argument1 vaList:args];
+    }
+
+    va_end(args);
 }
 
 @end
