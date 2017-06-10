579cbb29bbb8cf3ec876bcc1a5195e51ef1f0955
Tang Tianyong <tang3w@gmail.com>
2017-06-11 02:02:58 +0800

    Add umbrella header for connection module

---
+++ AVOS/Connection/AVOSCloudConnection.h
@@ -0,0 +1,11 @@
+//
+//  AVOSCloudConnection.h
+//  AVOSCloudConnection
+//
+//  Created by Tang Tianyong on 11/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "AVConnection.h"
