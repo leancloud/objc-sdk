8970919185dc177f8d84eec830b4c17d699e0eab
Tang Tianyong <tang3w@gmail.com>
2017-06-11 12:06:43 +0800

    Add prefix AVOSCloud to product name

--- AVOS/CrashReporting/CrashReporting.h
+++
@@ -1,9 +0,0 @@
-//
-//  CrashReporting.h
-//  CrashReporting_iOS
-//
-//  Created by Tang Tianyong on 11/06/2017.
-//  Copyright © 2017 LeanCloud Inc. All rights reserved.
-//
-
-#import "AVOSCloudCrashReporting.h"
--- AVOS/LiveQuery/LiveQuery.h
+++
@@ -1,9 +0,0 @@
-//
-//  LiveQuery.h
-//  AVOS
-//
-//  Created by Tang Tianyong on 11/06/2017.
-//  Copyright © 2017 LeanCloud Inc. All rights reserved.
-//
-
-#import "AVOSCloudLiveQuery.h"
