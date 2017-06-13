9f57b8b59d741137e1d09614a9d4c148422d7544
Tang Tianyong <tang3w@gmail.com>
2017-06-13 22:18:51 +0800

    Add project accessible umbrella header for foundation module

---
+++ Sources/Foundation/LCFoundation.h
@@ -0,0 +1,10 @@
+//
+//  LCFoundation.h
+//  AVOSCloud
+//
+//  Created by Tang Tianyong on 13/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "AVSDK.h"
+#import "LCDevice.h"
