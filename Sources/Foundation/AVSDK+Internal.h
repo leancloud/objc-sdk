74a56aba15b6acd99210f230b509bada5ddb5986
Tang Tianyong <tang3w@gmail.com>
2017-06-15 00:44:55 +0800

    Extract statistics data from analytics to foundation

---
+++ Sources/Foundation/AVSDK+Internal.h
@@ -0,0 +1,15 @@
+//
+//  AVSDK+Internal.h
+//  AVOSCloud
+//
+//  Created by Tang Tianyong on 15/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "AVSDK.h"
+
+@interface AVSDK (Internal)
+
+- (NSDictionary *)statisticsData;
+
+@end
---
+++ Sources/Foundation/AVSDK+Internal.m
@@ -0,0 +1,75 @@
+//
+//  AVSDK+Internal.m
+//  AVOSCloud
+//
+//  Created by Tang Tianyong on 15/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "AVSDK+Internal.h"
+#import "LCDevice.h"
+#import "LCApplication.h"
+
+#define LC_SDK_TYPE @"Objective-C"
+
+NS_INLINE
+NSString *LCStringFromCGSize(CGSize size) {
+    NSString *result = [NSString stringWithFormat:@"%ld x %ld", (long)size.width, (long)size.height];
+
+    return result;
+}
+
+NS_INLINE
+NSString *LCStringFromReachabilityStatus(LCNetworkReachabilityStatus status) {
+    NSString *result = nil;
+
+    switch (status) {
+    case LCNetworkReachabilityStatusUnknown:
+        break;
+    case LCNetworkReachabilityStatusNotReachable:
+        break;
+    case LCNetworkReachabilityStatusReachableViaWWAN:
+        result = @"WWAN";
+        break;
+    case LCNetworkReachabilityStatusReachableViaWiFi:
+        result = @"WiFi";
+        break;
+    }
+
+    return result;
+}
+
+@implementation AVSDK (Internal)
+
+- (NSDictionary *)statisticsData {
+    NSMutableDictionary *data = [NSMutableDictionary dictionary];
+
+    LCDevice *device = [LCDevice current];
+    LCApplication *application = [LCApplication current];
+
+    /* SDK basic data. */
+    data[@"sdk_type"]       = LC_SDK_TYPE;
+    data[@"sdk_version"]    = self.version;
+
+    /* Device related data. */
+    data[@"device_model"]   = device.model;
+    data[@"os"]             = device.systemName;
+    data[@"os_version"]     = device.systemVersion;
+    data[@"resolution"]     = LCStringFromCGSize(device.screenSize);
+    data[@"language"]       = device.language;
+    data[@"timezone"]       = device.timezone;
+    data[@"access"]         = LCStringFromReachabilityStatus(device.networkReachabilityStatus);
+    data[@"is_jailbroken"]  = @(device.jailbroken);
+#if LC_TARGET_OS_IOS
+    data[@"device_id"]      = device.UDID;
+#endif
+
+    /* Application related data. */
+    data[@"display_name"]   = application.name;
+    data[@"app_version"]    = application.shortVersion;
+    data[@"package_name"]   = application.identifier;
+
+    return data;
+}
+
+@end
