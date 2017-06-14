b5f8a141483c2931bda6e43b2d6236097fe487e4
Tang Tianyong <tang3w@gmail.com>
2017-06-14 12:56:50 +0800

    Add target conditional macros

--- Sources/Foundation/LCTargetConditionals.h
+++ Sources/Foundation/LCTargetConditionals.h
@@ -6,4 +6,26 @@
 //  Copyright © 2017 LeanCloud Inc. All rights reserved.
 //
 
+#ifdef TARGET_OS_IOS
+    #define LC_TARGET_OS_IOS TARGET_OS_IOS
+#else
+    #define LC_TARGET_OS_IOS 0
+#endif
 
+#ifdef TARGET_OS_OSX
+    #define LC_TARGET_OS_MAC TARGET_OS_OSX
+#else
+    #define LC_TARGET_OS_MAC 0
+#endif
+
+#ifdef TARGET_OS_TV
+    #define LC_TARGET_OS_TV TARGET_OS_TV
+#else
+    #define LC_TARGET_OS_TV 0
+#endif
+
+#ifdef TARGET_OS_WATCH
+    #define LC_TARGET_OS_WATCH TARGET_OS_WATCH
+#else
+    #define LC_TARGET_OS_WATCH 0
+#endif
