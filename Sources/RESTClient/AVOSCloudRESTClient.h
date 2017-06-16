d2333e60d100ec62a90718910971388fb779267f
Tang Tianyong <tang3w@gmail.com>
2017-06-16 15:17:39 +0800

    Add REST client module

---
+++ Sources/RESTClient/AVOSCloudRESTClient.h
@@ -0,0 +1,7 @@
+//
+//  AVOSCloudRESTClient.h
+//  RESTClient-iOS
+//
+//  Created by Tang Tianyong on 16/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
---
+++ Sources/RESTClient/RESTClient.h
@@ -0,0 +1,9 @@
+//
+//  RESTClient.h
+//  RESTClient
+//
+//  Created by Tang Tianyong on 16/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
