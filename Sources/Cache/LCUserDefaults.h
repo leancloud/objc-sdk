6e467fe8904e0e3840f8649dfc6e43a5bbec6674
Tang Tianyong <tang3w@gmail.com>
2017-06-17 21:02:07 +0800

    Add user defaults class

---
+++ Sources/Cache/LCUserDefaults.h
@@ -0,0 +1,13 @@
+//
+//  LCUserDefaults.h
+//  AVOSCloud
+//
+//  Created by Tang Tianyong on 17/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LCUserDefaults : NSObject
+
+@end
---
+++ Sources/Cache/LCUserDefaults.m
@@ -0,0 +1,13 @@
+//
+//  LCUserDefaults.m
+//  AVOSCloud
+//
+//  Created by Tang Tianyong on 17/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "LCUserDefaults.h"
+
+@implementation LCUserDefaults
+
+@end
