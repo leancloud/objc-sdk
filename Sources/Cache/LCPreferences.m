1e57b22cd3b054ef77b0ddb546bb6597604f8fce
Tang Tianyong <tang3w@gmail.com>
2017-06-17 22:09:40 +0800

    Add LCPreference to unify user defaults storage for all targets

---
+++ Sources/Cache/LCPreferences.m
@@ -0,0 +1,13 @@
+//
+//  LCPreferences.m
+//  AVOSCloud
+//
+//  Created by Tang Tianyong on 17/06/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "LCPreferences.h"
+
+@implementation LCPreferences
+
+@end
