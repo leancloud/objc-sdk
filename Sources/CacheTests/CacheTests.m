5650633a3292d7de1ea9f9d79a7f3f8b7febe93f
Tang Tianyong <tang3w@gmail.com>
2017-06-19 17:23:50 +0800

    Remove performance test case for cache module

--- Sources/CacheTests/CacheTests.m
+++ Sources/CacheTests/CacheTests.m
@@ -37,11 +37,4 @@
     XCTAssertEqualObjects(preferences[@"foo"], @"bar");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
