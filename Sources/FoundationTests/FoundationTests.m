66a951df664b6b0f70e2191a1d4c2af679399b2e
Tang Tianyong <tang3w@gmail.com>
2017-07-12 19:41:11 +0800

    Add test case for named table

--- Sources/FoundationTests/FoundationTests.m
+++ Sources/FoundationTests/FoundationTests.m
@@ -7,8 +7,40 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "AVNamedTable.h"
 
 @interface FoundationTests : XCTestCase
+
+@end
+
+@interface AVNamedTableSubclass : AVNamedTable
+
+@property (nonatomic, assign) NSInteger number;
+@property (nonatomic,   copy) NSString *string;
+@property (nonatomic, strong) id<NSCopying, NSSecureCoding> object;
+
+- (instancetype)initWithNumber:(NSInteger)number
+                        string:(NSString *)string
+                        object:(id<NSCopying, NSSecureCoding>)object;
+
+@end
+
+@implementation AVNamedTableSubclass
+
+- (instancetype)initWithNumber:(NSInteger)number
+                        string:(NSString *)string
+                        object:(id<NSCopying,NSSecureCoding>)object
+{
+    self = [super init];
+
+    if (self) {
+        _number = number;
+        _string = [string copy];
+        _object = object;
+    }
+
+    return self;
+}
 
 @end
 
@@ -24,16 +56,47 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
+- (void)testNamedTable {
+    NSInteger number = 42;
+    /* Use mutable string to make sure that it can be copied. */
+    NSString *string = [[NSMutableString alloc] initWithFormat:@"foo and bar"];
+    id        object = [NSArray array];
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    AVNamedTableSubclass *namedTable = [[AVNamedTableSubclass alloc] init];
+
+    namedTable.number = number;
+    namedTable.string = string;
+    namedTable.object = object;
+
+    XCTAssertEqual(namedTable.number, number);
+    XCTAssertEqualObjects(namedTable.string, string);
+    XCTAssertEqualObjects(namedTable.object, object);
+
+    /* Make sure that the copy property works as expected. */
+    XCTAssertNotEqual(namedTable.string, string);
+
+    AVNamedTableSubclass *namedTableCopy = [namedTable copy];
+
+    XCTAssertEqual(namedTableCopy.number, number);
+    XCTAssertEqualObjects(namedTableCopy.string, string);
+    XCTAssertEqualObjects(namedTableCopy.object, object);
+
+    /* Test encode and decode. */
+    NSData *archivedData = [NSKeyedArchiver archivedDataWithRootObject:namedTable];
+    AVNamedTableSubclass *decodedNamedTable = [NSKeyedUnarchiver unarchiveObjectWithData:archivedData];
+
+    XCTAssertEqual(decodedNamedTable.number, number);
+    XCTAssertEqualObjects(decodedNamedTable.string, string);
+    XCTAssertEqualObjects(decodedNamedTable.object, object);
+
+    /* Test instance variables work as expected. */
+    AVNamedTableSubclass *yaNamedTable = [[AVNamedTableSubclass alloc] initWithNumber:number
+                                                                               string:string
+                                                                               object:object];
+
+    XCTAssertEqual(yaNamedTable.number, number);
+    XCTAssertEqualObjects(yaNamedTable.string, string);
+    XCTAssertEqualObjects(yaNamedTable.object, object);
 }
 
 @end
