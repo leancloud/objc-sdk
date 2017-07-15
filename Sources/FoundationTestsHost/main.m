29e4bcbc9435136f875ab860c095cce67aa68c50
Tang Tianyong <tang3w@gmail.com>
2017-07-15 11:28:13 +0800

    Add tests host for foundation

---
+++ Sources/FoundationTestsHost/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  FoundationTestsHost
+//
+//  Created by Tang Tianyong on 15/07/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@end
+
+@implementation AppDelegate
+@end
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
