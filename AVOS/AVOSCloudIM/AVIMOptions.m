499febfa12c041d02e6d88abd5e1d6b215562c35
Tang Tianyong <tang3w@gmail.com>
2017-07-29 14:46:37 +0800

    Support to customize RTM server

---
+++ AVOS/AVOSCloudIM/AVIMOptions.m
@@ -0,0 +1,13 @@
+//
+//  AVIMOptions.m
+//  AVOS
+//
+//  Created by Tang Tianyong on 29/07/2017.
+//  Copyright © 2017 LeanCloud Inc. All rights reserved.
+//
+
+#import "AVIMOptions.h"
+
+@implementation AVIMOptions
+
+@end
