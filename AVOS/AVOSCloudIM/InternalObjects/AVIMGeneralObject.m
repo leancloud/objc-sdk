b49752620f5dddd1a07acadb350cbcf4571db0d8
Tang Tianyong <tang3w@gmail.com>
2017-08-20 23:04:15 +0800

    Synthesize properties of dynamic object explicitly

--- AVOS/AVOSCloudIM/InternalObjects/AVIMGeneralObject.m
+++ AVOS/AVOSCloudIM/InternalObjects/AVIMGeneralObject.m
@@ -9,5 +9,18 @@
 #import "AVIMGeneralObject.h"
 
 @implementation AVIMGeneralObject
-@dynamic width, height, size, duration, name, format, longitude, latitude, location, metaData, objId, url;
+
+LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (width,     setWidth, uint)
+LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (height,    setHeight, uint)
+LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (size,      setSize, uint64_t)
+LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (duration,  setDuration, float)
+LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (name,      setName)
+LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (format,    setFormat)
+LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (url,       setUrl)
+LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (objId,     setObjId)
+LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (longitude, setLongitude, float)
+LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (latitude,  setLatitude, float)
+LC_FORWARD_PROPERTY_ACCESSOR_OBJECT         (metaData, setMetaData)
+LC_FORWARD_PROPERTY_ACCESSOR_OBJECT         (location, setLocation)
+
 @end
