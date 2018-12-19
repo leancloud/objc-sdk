af4c1a9700a47a7efe3c8b4c06c8144390457c8c
zapcannon87 <zapcannon87@outlook.com>
2018-12-19 13:13:23 +0800

    Update runtime tests

---
+++ AVOS/RuntimeTestDemo/LiveQueryTestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LiveQueryTestViewController.swift
+//  RuntimeTestDemo
+//
+//  Created by zapcannon87 on 2018/12/19.
+//  Copyright © 2018 LeanCloud Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVOSCloud
+import AVOSCloudIM
+import AVOSCloudLiveQuery
+
+class LiveQueryTestViewController: UIViewController {
+    
+}
