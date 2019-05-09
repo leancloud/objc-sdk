27e7a960f8ee4214503a0141359abb303d691bdc
zapcannon87 <zapcannon87@outlook.com>
2019-05-09 15:49:29 +0800

    test: Runtime test for live query

--- AVOS/RuntimeTestDemo/LiveQueryTestViewController.swift
+++ AVOS/RuntimeTestDemo/LiveQueryTestViewController.swift
@@ -14,4 +14,35 @@ import AVOSCloudLiveQuery
 
 class LiveQueryTestViewController: UIViewController {
     
+    var liveQuery: AVLiveQuery!
+    
+    var query: AVQuery!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.query = AVQuery(className: "Todo")
+        self.query.whereKeyExists("objectId")
+        
+        self.liveQuery = AVLiveQuery(query: query)
+        self.liveQuery.delegate = self
+        
+        self.liveQuery.subscribe { (succeeded, error) in
+            
+        }
+    }
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        let object = AVObject(className: "Todo")
+        object.save()
+    }
+    
+}
+
+extension LiveQueryTestViewController: AVLiveQueryDelegate {
+    
+    func liveQuery(_ liveQuery: AVLiveQuery, objectDidCreate object: Any) {
+        print("\(liveQuery) objectDidCreate: \(object)")
+    }
+    
 }
