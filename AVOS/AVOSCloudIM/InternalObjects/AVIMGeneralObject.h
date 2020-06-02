9efedb60ace411339cb4f6193fed5327e3daf33b
zapcannon87 <zapcannon87@outlook.com>
2020-06-02 19:24:03 +0800

    fix(im): type checking of typed message

--- AVOS/AVOSCloudIM/InternalObjects/AVIMGeneralObject.h
+++ AVOS/AVOSCloudIM/InternalObjects/AVIMGeneralObject.h
@@ -9,16 +9,18 @@
 #import "AVIMDynamicObject.h"
 
 @interface AVIMGeneralObject : AVIMDynamicObject
-@property(nonatomic, assign) uint width;
-@property(nonatomic, assign) uint height;
-@property(nonatomic, assign) uint64_t size;
-@property(nonatomic, assign) float duration;
-@property(nonatomic,   copy) NSString *name;
-@property(nonatomic,   copy) NSString *format;
-@property(nonatomic,   copy) NSString *url;
-@property(nonatomic,   copy) NSString *objId;
-@property(nonatomic, assign) float longitude;
-@property(nonatomic, assign) float latitude;
-@property(nonatomic, strong) AVIMGeneralObject *metaData;
-@property(nonatomic, strong) AVIMGeneralObject *location;
+
+@property (nonatomic) double width;
+@property (nonatomic) double height;
+@property (nonatomic) double size;
+@property (nonatomic) double duration;
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSString *format;
+@property (nonatomic) NSString *url;
+@property (nonatomic) NSString *objId;
+@property (nonatomic) double longitude;
+@property (nonatomic) double latitude;
+@property (nonatomic) AVIMGeneralObject *metaData;
+@property (nonatomic) AVIMGeneralObject *location;
+
 @end
--- AVOS/AVOSCloudIM/InternalObjects/AVIMGeneralObject.m
+++ AVOS/AVOSCloudIM/InternalObjects/AVIMGeneralObject.m
@@ -7,20 +7,137 @@
 //
 
 #import "AVIMGeneralObject.h"
+#import "AVUtils.h"
 
 @implementation AVIMGeneralObject
 
-LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (width,     setWidth, uint)
-LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (height,    setHeight, uint)
-LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (size,      setSize, uint64_t)
-LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (duration,  setDuration, float)
-LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (name,      setName)
-LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (format,    setFormat)
-LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (url,       setUrl)
-LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (objId,     setObjId)
-LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (longitude, setLongitude, float)
-LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (latitude,  setLatitude, float)
-LC_FORWARD_PROPERTY_ACCESSOR_OBJECT         (metaData, setMetaData)
-LC_FORWARD_PROPERTY_ACCESSOR_OBJECT         (location, setLocation)
+//LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (width,     setWidth, uint)
+//LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (height,    setHeight, uint)
+//LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (size,      setSize, uint64_t)
+//LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (duration,  setDuration, float)
+//LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (name,      setName)
+//LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (format,    setFormat)
+//LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (url,       setUrl)
+//LC_FORWARD_PROPERTY_ACCESSOR_OBJECT_COPY    (objId,     setObjId)
+//LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (longitude, setLongitude, float)
+//LC_FORWARD_PROPERTY_ACCESSOR_NUMBER         (latitude,  setLatitude, float)
+//LC_FORWARD_PROPERTY_ACCESSOR_OBJECT         (metaData, setMetaData)
+//LC_FORWARD_PROPERTY_ACCESSOR_OBJECT         (location, setLocation)
+
+// MARK: NSNumber
+
+- (double)width {
+    return [NSNumber _lc_decoding:self.localData key:@"width"].doubleValue;
+}
+
+- (void)setWidth:(double)width {
+    [self setObject:@(width) forKey:@"width"];
+}
+
+- (double)height {
+    return [NSNumber _lc_decoding:self.localData key:@"height"].doubleValue;
+}
+
+- (void)setHeight:(double)height {
+    [self setObject:@(height) forKey:@"height"];
+}
+
+- (double)size {
+    return [NSNumber _lc_decoding:self.localData key:@"size"].doubleValue;
+}
+
+- (void)setSize:(double)size {
+    [self setObject:@(size) forKey:@"size"];
+}
+
+- (double)duration {
+    return [NSNumber _lc_decoding:self.localData key:@"duration"].doubleValue;
+}
+
+- (void)setDuration:(double)duration {
+    [self setObject:@(duration) forKey:@"duration"];
+}
+
+- (double)latitude {
+    return [NSNumber _lc_decoding:self.localData key:@"latitude"].doubleValue;
+}
+
+- (void)setLatitude:(double)latitude {
+    [self setObject:@(latitude) forKey:@"latitude"];
+}
+
+- (double)longitude {
+    return [NSNumber _lc_decoding:self.localData key:@"longitude"].doubleValue;
+}
+
+- (void)setLongitude:(double)longitude {
+    [self setObject:@(longitude) forKey:@"longitude"];
+}
+
+// MARK: NSString
+
+- (NSString *)name {
+    return [NSString _lc_decoding:self.localData key:@"name"];
+}
+
+- (void)setName:(NSString *)name {
+    [self setObject:name forKey:@"name"];
+}
+
+- (NSString *)format {
+    return [NSString _lc_decoding:self.localData key:@"format"];
+}
+
+- (void)setFormat:(NSString *)format {
+    [self setObject:format forKey:@"format"];
+}
+
+- (NSString *)url {
+    return [NSString _lc_decoding:self.localData key:@"url"];
+}
+
+- (void)setUrl:(NSString *)url {
+    [self setObject:url forKey:@"url"];
+}
+
+- (NSString *)objId {
+    return [NSString _lc_decoding:self.localData key:@"objId"];
+}
+
+- (void)setObjId:(NSString *)objId {
+    [self setObject:objId forKey:@"objId"];
+}
+
+// MARK: AVIMGeneralObject
+
+- (AVIMGeneralObject *)metaData {
+    NSDictionary *object = [NSDictionary _lc_decoding:self.localData key:@"metaData"];
+    if (object) {
+        return [[AVIMGeneralObject alloc] initWithDictionary:object];
+    } else if ([object isKindOfClass:[AVIMGeneralObject class]]) {
+        return (AVIMGeneralObject *)object;
+    } else {
+        return nil;
+    }
+}
+
+- (void)setMetaData:(AVIMGeneralObject *)metaData {
+    [self setObject:[metaData dictionary] forKey:@"metaData"];
+}
+
+- (AVIMGeneralObject *)location {
+    NSDictionary *object = [NSDictionary _lc_decoding:self.localData key:@"location"];
+    if (object) {
+        return [[AVIMGeneralObject alloc] initWithDictionary:object];
+    } else if ([object isKindOfClass:[AVIMGeneralObject class]]) {
+        return (AVIMGeneralObject *)object;
+    } else {
+        return nil;
+    }
+}
+
+- (void)setLocation:(AVIMGeneralObject *)location {
+    [self setObject:[location dictionary] forKey:@"location"];
+}
 
 @end
