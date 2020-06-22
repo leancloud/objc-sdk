452df67bfa7699f36c92cebdd8191183ffb1111a
zapcannon87 <zapcannon87@outlook.com>
2020-06-22 18:05:21 +0800

    chore: new target and test

---
+++ AVOS/LeanCloudObjc/LeanCloudObjc.h
@@ -0,0 +1,23 @@
+//
+//  LeanCloudObjc.h
+//  LeanCloudObjc
+//
+//  Created by pzheng on 2020/06/22.
+//  Copyright © 2020 LeanCloud Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AVOSCloud.h"
+#import "AVOSCloudIM.h"
+#import "AVOSCloudLiveQuery.h"
+
+
+//! Project version number for LeanCloudObjc.
+FOUNDATION_EXPORT double LeanCloudObjcVersionNumber;
+
+//! Project version string for LeanCloudObjc.
+FOUNDATION_EXPORT const unsigned char LeanCloudObjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeanCloudObjc/PublicHeader.h>
+
+
---
+++ AVOS/LeanCloudObjcTests/BaseTestCase.swift
@@ -0,0 +1,14 @@
+//
+//  BaseTestCase.swift
+//  LeanCloudObjcTests
+//
+//  Created by pzheng on 2020/06/22.
+//  Copyright © 2020 LeanCloud Inc. All rights reserved.
+//
+
+import XCTest
+import LeanCloudObjc
+
+class BaseTestCase: XCTestCase {
+    
+}
