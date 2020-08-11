cf162df8aeb11f79d468ddc48bf5db1e6df96c08
zapcannon87 <zapcannon87@outlook.com>
2020-08-11 11:34:12 +0800

    docs: comments of files for generating API docs

--- AVOS/LeanCloudObjc/AVOSCloud.h
+++ AVOS/LeanCloudObjc/AVOSCloud.h
@@ -1,3 +1,13 @@
+/*
+ WARNING!!!
+ WARNING!!!
+ WARNING!!!
+ 
+ DO NOT remove or delete this file unless you are quite sure,
+ Keep it for generating API docs,
+ If you want more details, see https://github.com/realm/jazzy
+ */
+
 #import <Foundation/Foundation.h>
 
 // Public headers
