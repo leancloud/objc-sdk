da72164d0550bab7b7db644418a2b36e6385c9c1
zapcannon87 <zapcannon87@outlook.com>
2021-04-30 18:01:19 +0800

    chore: update headers

--- AVOS/AVOSCloudIM/MessageCache/LCIMMessageCache.m
+++ AVOS/AVOSCloudIM/MessageCache/LCIMMessageCache.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 LeanCloud Inc. All rights reserved.
 //
 
-#import <AVOSCloud/AVOSCloud.h>
+#import "AVOSCloud.h"
 
 #import "LCPersistenceUtils.h"
 #import "LCIMMessageCache.h"
